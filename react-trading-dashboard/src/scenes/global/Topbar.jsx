2433558478d61c87fd2837e921d33dd6b43063b1
Artur Abgaryan <arty219603@gmail.com>
2024-03-05 18:42:18 +0400

    started developing trading dashboard

---
+++ react-trading-dashboard/src/scenes/global/Topbar.jsx
@@ -0,0 +1,34 @@
+import { Box, Icon, IconButton, Typography, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import { InputBase } from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SearchIcon from "@mui/icons-material/DarkModeOutlined";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" width="100%" p={2}>
+      {/* Search bar */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
